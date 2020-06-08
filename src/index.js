c56c97a4559f9e4c56125997a7525932d9c78d4f
Diego Candido <diegocndd4@gmail.com>
2020-06-08 16:54:09 -0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text, Button, StyleSheet, Dimensions} from 'react-native';
 import Display from './components/Display';
-import Box from './components/Box';
 
 export default class App extends Component{
     
@@ -9,7 +8,6 @@ export default class App extends Component{
         return(
             <View>
                 <Display />
-                <Box />
             </View>
         )
     }
